ca9b1006da4f5551b6e029da6a87705d79d419e1
LivingtonMerello98 <livington.merello@gmail.com>
2024-09-08 11:25:14 +0200

    [upd] color Palettes in variables

--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import './style.scss' // Assicurati che il file SCSS sia qui
 import App from './App.vue';
 
 // Importa Poppins (ad esempio pesi 400 e 700)
+import '@fontsource/poppins/100.css';
+import '@fontsource/poppins/200.css';
+import '@fontsource/poppins/300.css';
 import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
+import '@fontsource/poppins/800.css';
 
 // Bootstrap
 
