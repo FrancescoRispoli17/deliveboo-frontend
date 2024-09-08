68ebbf7f621c3e06d2f29a26832fb798332813bc
LivingtonMerello98 <livington.merello@gmail.com>
2024-09-08 13:11:11 +0200

    [upd] header with filled mediaquery optimized

---
+++ src/components/headers/HeaderComponent.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+    name:'HeaderComponent',
+
+    data(){
+        return{}
+    },
+    methods: {
+
+    }
+}
+
+
+</script>
+
+<template>
+
+    <header class="bg-light">
+        <div class="container py-3">
+            <div class="d-flex">
+
+                <div class="col-lg-2 col-sm-6 bg-danger py-3 border border-dark side-div">
+                    <!-- logo -->
+                </div>
+                <div class="col-lg-8 bg-success search-bar-desktop ">
+                    <!-- search-bar per desktop -->
+                </div>
+                <div class="col-lg-2 col-sm-6   bg-danger py-3 border border-dark side-div">
+                    <!-- logo -->
+                </div>
+
+            </div>
+            <!-- search-bar tablet -->
+            <div class="col-md-12 bg-success py-3 d-lg-none d-{sm, md}-block">
+                    <!-- search-bar -->
+            </div>
+        </div>
+    </header>
+
+</template>
+
+<style lang="scss" scoped>
+
+@media screen and (max-width: 991px) {
+    .search-bar-desktop{
+    
+        display: none;
+    }
+}
+
+@media screen and (max-width: 573px) {
+    .side-div{
+        width: 50%;
+    }
+}
+
+</style>
--- src/pages/DesignSystem.vue
+++ src/pages/DesignSystem.vue
@@ -1,22 +1,41 @@
 <script>
 
+import HeaderComponent from '../components/headers/HeaderComponent.vue';
+
 export default {
-    name: 'DesignSystem'
+    name: 'DesignSystem',
+
+    components:{
+        HeaderComponent
+    },
 }
 
 </script>
 
 <template>
-    <div class="container py-5">
+    <section class="mb-3">
+        <div class="container py-5">
         <div class="row">
             <div class="col-md-12">
                 <h3 class="text-light mb-3">Design system</h3>
                 <p class="text-light fw-light">Questa è la pagina dedicata al design system <br>
                     qui potrete trovare una lista di componenti globali e riutilizzabili in modo <br>
-                    che il progetto possa mantenere una stilistica</p>
+                    che il progetto possa mantenere una coerenza stilistica</p>
             </div>
         </div>
     </div>
+
+    <!-- header -->
+    <div class="container">
+        <div class="col md-12">
+            <h5 class="text-light">Header white filled</h5>
+        </div>
+    </div>
+    <HeaderComponent/>
+    <!-- header -->
+
+
+    </section>
 </template>
 
 <style lang="scss" scoped></style>
