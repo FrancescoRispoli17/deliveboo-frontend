5e1e08bb28308457ff64180ab47511ae5590a96d
LivingtonMerello98 <livington.merello@gmail.com>
2024-09-11 15:10:44 +0200

    [UPD] CardHeroComponent created

--- src/components/hero/CardHeroComponent.vue
+++ src/components/hero/CardHeroComponent.vue
@@ -21,7 +21,7 @@ export default {
 
 <template>
 
-    <div class="container">
+    <div class="container py-5">
         <div class="row">
             <div class="col-md-12 text-center mb-4">
                 <h1 class="title">
@@ -31,17 +31,18 @@ export default {
             <div class="col-md-12">
                 <div class="custom-card">
 
-                    <p class="fw-normal mb-3">
+                    <h1 class="title-sm mb-3">
+                    I piatti dei ristoranti che ami e la spesa, a domicilio
+                    </h1>
+
+                    <p class="label mb-3">
                         Inserisci un indirizzo per scoprire le opzioni disponibili nella tua zona
                     </p>
-
                     <!-- input address -->
                      <InputAddressComponent/>
 
                 </div>
             </div>
-
-
         </div>
     </div>
 
@@ -52,31 +53,59 @@ export default {
 @use 'src/assets/partials/_mixin.scss' as *;
 
 .title{
-    font-size: 2.5rem;
-    font-weight: 600;
+    font-size: 2.8rem;
+    font-weight: 700;
+    line-height: 3.5rem;
+}
+
+.title-sm{
+    display: none;
 }
 
 .custom-card{
     padding: 2.5rem;
     background-color: $quaternary-color;
-    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);  
+    box-shadow: 0 6px 6px  rgba(0, 0, 0, 0.1);  
     @include border-solid
 }
+
 //tablet
 @media screen and (max-width: 768px) {
 
     .title{
         font-size: 1.5rem;
         text-align: start;
+        line-height: 2rem;
+    }
+
+    .title-sm{
+        display:none;
+    }
+
+    .label{
+        font-size: 0.9rem;
     }
 
 }
+
+
 //smartphone
 @media screen and (max-width: 425px) {
 
     .title{
-        font-size: 1.5rem;
-        text-align: center;
+        display: none;
+    }
+
+    .title-sm{
+        font-size: 1.2rem;
+        text-align: start;
+        display:block;
+        font-weight: 600;
+    }
+
+    .label{
+        font-size: 1rem;
+        display: block;
     }
 
 }
