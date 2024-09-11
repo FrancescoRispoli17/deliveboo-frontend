10d88bdd0cddf2398b4f199e65ab51132a1c8214
LivingtonMerello98 <livington.merello@gmail.com>
2024-09-11 15:12:28 +0200

    [UPD] DesignSystem created

--- src/pages/DesignSystem.vue
+++ src/pages/DesignSystem.vue
@@ -3,6 +3,11 @@
 import HeaderComponent from '../components/headers/HeaderComponent.vue';
 import HeroComponent from '../components/hero/HeroComponent.vue';
 import MapCardComponent from '../components/cards/MapCardComponent.vue';
+import CarouselComponent from '../components/containers/CarouselComponent.vue';
+import FooterComponent from '../components/footers/FooterComponent.vue';
+import ServiceComponent from '../components/containers/ServiceComponent.vue';
+import OffersComponent from '../components/containers/OffersComponent.vue';
+import CarouselContainerComponent from '../components/containers/CarouselContainerComponent.vue';
 
 export default {
     name: 'DesignSystem',
@@ -10,7 +15,12 @@ export default {
     components:{
         HeaderComponent,
         HeroComponent,
-        MapCardComponent
+        MapCardComponent,
+        CarouselComponent,
+        CarouselContainerComponent,
+        ServiceComponent,
+        OffersComponent,
+        FooterComponent
     },
 }
 
@@ -33,6 +43,7 @@ export default {
     <div class="container">
         <div class="col md-12">
             <h5 class="text-light">Header white filled</h5>
+            <p class="text-light">Header presente all'interno del sito, dopo la home page</p>
         </div>
     </div>
     <div class="mb-3">
@@ -43,12 +54,14 @@ export default {
     <div class="container">
         <div class="col md-12">
             <h5 class="text-light">Hero with header transparent</h5>
+            <p class="text-light">Hero presente in Hompage, con all interno il componente Header Transaparente</p>
         </div>
     </div>
     <div class="mb-3">
        <HeroComponent/> 
     </div>
-    <!-- hero -->
+
+    <!-- MapCard -->
     <div class="container">
         <div class="col md-12">
             <h5 class="text-light">Map Card</h5>
@@ -58,8 +71,55 @@ export default {
        <MapCardComponent/> 
     </div>
 
+    <!-- carosello -->
+    <div class="container">
+        <div class="col md-12">
+            <h5 class="text-light">Container Caroselli</h5>
+            <p class="text-light">Carosello in Homepage</p>
+        </div>
+    </div>
+    <div>
+        <CarouselContainerComponent/>
+    </div>
+
+
+    <!-- service component -->
+    <div class="container mt-3">
+        <div class="col md-12">
+            <h5 class="text-light">Offers Component</h5>
+            <p class="text-light">sezione offerta</p>
+        </div>
+    </div>
+    <section>
+        <OffersComponent/>
+    </section>
+
+    <!-- service component -->
+    <div class="container mt-3">
+        <div class="col md-12">
+            <h5 class="text-light">Service component</h5>
+            <p class="text-light">sezione service con card</p>
+        </div>
+    </div>
+    <section>
+        <ServiceComponent/>
+    </section>
+
+    <!-- cards -->
+    <div class="container mt-3">
+        <div class="col md-12">
+            <h5 class="text-light">Footer</h5>
+            <p class="text-light">Footer HomePage</p>
+        </div>
+    </div>
+    <section>
+        <FooterComponent/>
+    </section>
 
     </section>
+
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
