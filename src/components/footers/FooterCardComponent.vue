4c26e84c34dbab20264a45e2a83a0ac2ca42901a
LivingtonMerello98 <livington.merello@gmail.com>
2024-09-11 15:05:37 +0200

    [UPD] FooterCardComponent created

---
+++ src/components/footers/FooterCardComponent.vue
@@ -0,0 +1,112 @@
+
+<script>
+export default {
+    name: 'FooterCardComponent' ,
+
+    data(){
+    return{
+        cards:[
+            {
+                title: 'Scopri Booliveroo',
+                links:[
+                    'investitori',
+                    'Chi Siamo',
+                    'Ristoranti',
+                    'Altro',
+                    'Pressroom',
+                    'Programmazione',
+                    'Design',
+                    'Lavora con noi',
+                    'Diventa nostro partner'
+                ]
+            },
+            {               
+                title: 'Note legali',
+                links:[
+                    'Termini e condizioni',
+                    'informazioni sulla privacy',
+                    'Ristoranti',
+                    'Cookies',
+                    'Classificazione dei partner',
+                    'Richieste da parte delle Autorità',
+                    'Pubbliche',
+                ]
+            },
+            {                
+                title: 'Aiuto',
+                links:[
+                    'Contatti',
+                    'FAQ',
+                    'Tipi di cucina',
+                ]
+            },
+            {
+                title: 'Porta Boolivero con te',
+                links:[],
+                image: 'https://nuovisogni.it/wp-content/uploads/2020/07/app-badge.png',
+            }
+        ]
+    }
+    },
+    components: {
+
+    },
+    methods: {
+        
+    }
+}
+
+
+</script>
+
+<template>
+    <div class="row">
+      <div v-for="(card, index) in cards" :key="index" class="col-lg-3 col-md-6 col-sm-12 p-2">
+        <div class="col-md-12 bg-card ">
+          <!-- Titolo della card -->
+          <p class="text-light title">{{ card.title }}</p>
+          
+          <!-- Lista di link -->
+          <ul v-if="card.links.length">
+
+            <li v-for="(link, index) in card.links" :key="index" class="link">
+              {{ link }}
+            </li>
+
+          </ul>
+  
+          <!-- Mostra immagine se esiste -->
+          <div v-if="card.image">
+            <img :src="card.image" alt="App badge" class="cta" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+
+<style lang="scss" scoped>
+@use 'src/assets/partials/_variables.scss' as *;
+@use 'src/assets/partials/_mixin.scss' as *;
+
+.title{
+    font-size: 1.1rem ;
+    font-weight:700;
+}
+
+.bg-card{
+    background-color: $tertiary-color;
+    color: $quaternary-color;
+    padding:1.2rem;
+    height: 100%;
+    border-radius: 0.2rem;
+}
+
+.link{
+    @include link
+}
+
+.cta{
+    width:60%;
+}
+
+</style>
