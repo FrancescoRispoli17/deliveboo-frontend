d0c3f8973cdc1350979be99c78e84623f8d25c27
LivingtonMerello98 <livington.merello@gmail.com>
2024-09-11 15:03:18 +0200

    [UPD] LogoFilledComponent created

---
+++ src/components/containers/LogoFilledComponent.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    name: 'LogoFilledComponent',
+
+    data(){
+    return{}
+    },
+    component: {
+
+    },
+    methods: {
+        
+    }
+}
+
+
+</script>
+
+<template>
+
+    <div class="logo-container">
+        <img src="/logo_bg-filled.png" alt="">
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use 'src/assets/partials/_variables.scss' as*;
+@use 'src/assets/partials/_mixin.scss' as*;
+
+.logo-container{
+    width: 90%;
+
+    img{
+        width: 65%;
+        // box-shadow: 0 6px 6px rgb(0, 0, 0, 0.1);
+    }
+}
+
+@media screen and (max-width: 991px) {
+    .logo-container{
+    
+        width: 100%;
+    }
+
+    img{
+        width: 100%;
+    }
+}
+
+</style>
