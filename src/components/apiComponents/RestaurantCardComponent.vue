d2cc008e9dc9471c9a2995a87136512bd4b5c6dd
LivingtonMerello98 <livington.merello@gmail.com>
2024-09-12 15:16:03 +0200

    [UPD] RestaurantCardComponent

---
+++ src/components/apiComponents/RestaurantCardComponent.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'RestaurantCardComponent' ,
+
+    data(){
+    return{}
+    },
+    components: {
+
+    },
+    methods: {
+        
+    }
+}
+
+
+</script>
+
+<template>
+
+    <h1 class="text-light">io sono la card</h1>
+
+</template>
+
+<style lang="scss" scoped>
+@use 'src/assets/partials/_variables.scss' as *;
+@use 'src/assets/partials/_mixin.scss' as *;
+
+</style>
