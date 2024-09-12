2ab29d012a6262778347d3043112befff7a18e97
LivingtonMerello98 <livington.merello@gmail.com>
2024-09-12 21:30:31 +0200

    [UPD] RestaurantCardCompent

--- src/components/apiComponents/RestaurantCardComponent.vue
+++ src/components/apiComponents/RestaurantCardComponent.vue
@@ -1,15 +1,35 @@
 <script>
+import axios from 'axios';
+import { store } from '../../store';
+
 export default {
     name: 'RestaurantCardComponent' ,
 
     data(){
-    return{}
+    return{
+        results:[],
+        apiRestaurant: `${store.url}${store.restaurants}`
+    }
     },
     components: {
 
     },
     methods: {
-        
+        getRestaurant(){
+            console.log('chiamata efettuata API URL: ', this.apiRestaurant);
+
+            axios.get(this.apiRestaurant)
+            .then( response => {
+                console.log('ristoranti: ', response.data)
+
+                //inseriamo response.data dentro l'array result
+                this.results = response.data;
+            })
+
+        }
+    },
+    mounted(){
+        this.getRestaurant()
     }
 }
 
@@ -17,10 +37,30 @@ export default {
 </script>
 
 <template>
-
-    <h1 class="text-light">io sono la card</h1>
-
+    <div class="container">
+        <div class="row">
+            <!-- Itera su ciascun ristorante per creare una card -->
+            <div v-for="restaurant in results" :key="restaurant.id" class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title">{{ restaurant.name }}</h2>
+                        <p class="card-text">{{ restaurant.description }}</p>
+                        <p class="card-text fw-bold     ">{{ restaurant.city }}</p>
+                        <ul>
+                            <!-- Lista dei tipi di ristorante -->
+                            <li v-for="type in restaurant.types" :key="type.id">{{ type.name }}</li>
+                        </ul>
+                        <!-- Aggiungi un button o un link per accedere ai dettagli -->
+                        <router-link :to="{ name: 'dishes', params: { slug: restaurant.slug } }" class="btn btn-primary">
+                        Vedi Piatti
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
 
 <style lang="scss" scoped>
 @use 'src/assets/partials/_variables.scss' as *;
