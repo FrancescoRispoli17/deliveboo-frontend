562f4f87176cafe2f7eeedeee029added99308db
LivingtonMerello98 <livington.merello@gmail.com>
2024-09-13 11:58:31 +0200

    [UPD]RestaurantCardComponent  fails to get the data passed by the emit of TypeFilterComponent

--- src/components/apiComponents/RestaurantCardComponent.vue
+++ src/components/apiComponents/RestaurantCardComponent.vue
@@ -1,69 +1,84 @@
 <script>
 import axios from 'axios';
 import { store } from '../../store';
+import TypeFilterComponent from './TypeFilterComponent.vue';
+
 
 export default {
-    name: 'RestaurantCardComponent' ,
+  name: 'RestaurantCardComponent',
+  data() {
+    return {
+      results: [],
+      apiRestaurant: `${store.url}${store.restaurants}`,
+      selectedTypes: [], // Definisci selectedTypes qui
+    };
+  },
 
-    data(){
-    return{
-        results:[],
-        apiRestaurant: `${store.url}${store.restaurants}`
-    }
+  components: {
+    TypeFilterComponent,
+  },
+
+  methods: {
+    // Chiamata API per ottenere i ristoranti filtrati
+    getRestaurants() {
+      let filterUrl = this.apiRestaurant;
+
+      if (this.selectedTypes.length > 0) {
+        const typesQuery = this.selectedTypes.join(',');
+
+        filterUrl += `?types=${typesQuery}`;
+      }
+
+      axios.get(filterUrl)
+        .then(response => {
+            this.results = response.data;
+            console.log('Risposta API:', response.data); // Debugging
+        })
+        .catch(error => {
+          console.error('Errore nel recupero dei ristoranti:', error);
+        });
     },
-    components: {
 
+    // Metodo chiamato quando i tipi selezionati vengono aggiornati
+    handleFilterUpdate(selectedTypes) {
+      this.selectedTypes = selectedTypes; // Aggiorna il valore di selectedTypes
+      console.log('attualmente sono stati selezionati',this.selectedTypes)
+      this.getRestaurants(); // Aggiorna i ristoranti in base ai filtri
     },
-    methods: {
-        getRestaurant(){
-            console.log('chiamata efettuata API URL: ', this.apiRestaurant);
-
-            axios.get(this.apiRestaurant)
-            .then( response => {
-                console.log('ristoranti: ', response.data)
-
-                //inseriamo response.data dentro l'array result
-                this.results = response.data;
-            })
-
-        }
-    },
-    mounted(){
-        this.getRestaurant()
-    }
-}
-
-
+  },
+  mounted() {
+    this.getRestaurants();
+  },
+};
 </script>
 
 <template>
-    <div class="container">
-        <div class="row">
-            <!-- Itera su ciascun ristorante per creare una card -->
-            <div v-for="restaurant in results" :key="restaurant.id" class="col-md-4 mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h2 class="card-title">{{ restaurant.name }}</h2>
-                        <p class="card-text">{{ restaurant.description }}</p>
-                        <p class="card-text fw-bold     ">{{ restaurant.city }}</p>
-                        <ul>
-                            <!-- Lista dei tipi di ristorante -->
-                            <li v-for="type in restaurant.types" :key="type.id">{{ type.name }}</li>
-                        </ul>
-                        <!-- Aggiungi un button o un link per accedere ai dettagli -->
-                        <router-link :to="{ name: 'dishes', params: { slug: restaurant.slug } }" class="btn btn-primary">
-                        Vedi Piatti
-                        </router-link>
-                    </div>
-                </div>
-            </div>
+  <div class="container">
+    <TypeFilterComponent :selectedTypes="selectedTypes" @filterTypes="handleFilterUpdate" />
+
+    
+    <div class="row">
+      <div v-for="restaurant in results" :key="restaurant.id" class="col-md-4 mb-3">
+        <div class="card">
+          <div class="card-body">
+            <h2 class="card-title">{{ restaurant.name }}</h2>
+            <p class="card-text">{{ restaurant.description }}</p>
+            <p class="card-text fw-bold">{{ restaurant.city }}</p>
+            <ul>
+              <li v-for="type in restaurant.types" :key="type.id">{{ type.name }}</li>
+            </ul>
+            <router-link :to="{ name: 'dishes', params: { slug: restaurant.slug } }" class="btn btn-primary">
+              Vedi Piatti
+            </router-link>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
-
-<style lang="scss" scoped>
-@use 'src/assets/partials/_variables.scss' as *;
-@use 'src/assets/partials/_mixin.scss' as *;
-
+<style scoped>
+  .card {
+    margin-bottom: 20px;
+  }
 </style>
