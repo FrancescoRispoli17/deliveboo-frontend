34fbb546747f20e853ccb72ad7dafa6bef949412
rinox3005 <peppe.rinox@gmail.com>
2024-09-18 11:30:52 +0200

    [FIX] form validation and payment validation now works

--- src/components/apiComponents/BraintreePayment.vue
+++ src/components/apiComponents/BraintreePayment.vue
@@ -1,145 +1,188 @@
 <template>
-    <div>
-        <div id="dropin-container"></div>
-        <button @click="processPayment" class="btn btn-primary py-2 mb-2">Paga ora</button>
+  <div>
+    <div id="dropin-container"></div>
+    <button @click="processPayment" class="btn btn-primary py-2 mb-2">
+      Paga ora
+    </button>
 
-        <!-- modale bootstrap per pagamento riuscito -->
-        <div class="modal fade" id="paymentSuccessModal" tabindex="-1" aria-labelledby="paymentSuccessModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="paymentSuccessModalLabel">Pagamento Completato</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Il pagamento è stato completato con successo!
-                        <br />
-                        ID transazione: {{ transactionId }}
-                    </div>
-                    <div class="modal-footer">
-                        <a href="/restaurant" class="btn btn-primary" @click="handleRedirect">OK</a>
-                    </div>
-                </div>
-            </div>
+    <!-- Modale per pagamento riuscito -->
+    <div
+      class="modal fade"
+      id="paymentSuccessModal"
+      tabindex="-1"
+      aria-labelledby="paymentSuccessModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="paymentSuccessModalLabel">
+              Pagamento Completato
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            Il pagamento è stato completato con successo! <br />
+            ID transazione: {{ transactionId }}
+          </div>
+          <div class="modal-footer">
+            <a
+              href="/restaurant"
+              class="btn btn-primary"
+              @click="handleRedirect"
+              >OK</a
+            >
+          </div>
         </div>
-
-        <!-- modale bootstrap per pagamento fallito -->
-        <div class="modal fade" id="paymentErrorModal" tabindex="-1" aria-labelledby="paymentErrorModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="paymentErrorModalLabel">Errore di Pagamento</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Si è verificato un errore durante il pagamento. Per favore, riprova.
-                        <br />
-                        Errore: {{ errorMessage }}
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-</template>
-  
-  
-<script>
-import axios from 'axios';
-import dropin from 'braintree-web-drop-in';
-import { Modal } from 'bootstrap';
 
+    <!-- Modale per pagamento fallito -->
+    <div
+      class="modal fade"
+      id="paymentErrorModal"
+      tabindex="-1"
+      aria-labelledby="paymentErrorModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="paymentErrorModalLabel">
+              Errore di Pagamento
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            Si è verificato un errore durante il pagamento. Per favore, riprova.
+            <br />
+            Errore: {{ errorMessage }}
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal">
+              Chiudi
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import dropin from "braintree-web-drop-in";
+import { Modal } from "bootstrap";
 
 export default {
+  props: {
+    total: {
+      type: Number,
+      required: true,
+    },
+    formData: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      clientToken: "",
+      dropinInstance: null,
+      transactionId: "",
+      errorMessage: "",
+    };
+  },
+  async mounted() {
+    this.getClientToken();
+  },
+  methods: {
+    async getClientToken() {
+      try {
+        const response = await axios.get("/api/payment/token");
+        this.clientToken = response.data.token;
 
-    props: {
-        total: {
-            type: Number,
-            required: true
+        if (this.clientToken) {
+          this.initializeBraintree();
+        } else {
+          console.error("Errore: Il token Braintree non è stato ricevuto.");
         }
+      } catch (error) {
+        console.error("Errore nel recuperare il token:", error);
+      }
     },
-    data() {
-        return {
-            clientToken: '',
-            dropinInstance: null,
-            transactionId: '',
-            errorMessage: '',
-        };
-    },
-    async mounted() {
-        this.getClientToken();
-    },
-    methods: {
-        async getClientToken() {
-            try {
-                const response = await axios.get('/api/payment/token');
-                this.clientToken = response.data.token;
-
-                if (this.clientToken) {
-                    this.initializeBraintree();
-                } else {
-                    console.error('Errore: Il token Braintree non è stato ricevuto.');
-                }
-            } catch (error) {
-                console.error('Errore nel recuperare il token:', error);
-            }
+    initializeBraintree() {
+      dropin.create(
+        {
+          authorization: this.clientToken,
+          container: "#dropin-container",
         },
-        initializeBraintree() {
-            dropin.create(
-                {
-                    authorization: this.clientToken,
-                    container: '#dropin-container',
-                },
-                (createErr, instance) => {
-                    if (createErr) {
-                        console.error("Errore nell'inizializzazione di Braintree:", createErr);
-                        return;
-                    }
-                    this.dropinInstance = instance;
-                }
+        (createErr, instance) => {
+          if (createErr) {
+            console.error(
+              "Errore nell'inizializzazione di Braintree:",
+              createErr
             );
-        },
-        processPayment() {
-            if (!this.dropinInstance) {
-                console.error("L'istanza di Braintree Drop-In non è stata inizializzata.");
-                return;
-            }
-
-            this.dropinInstance.requestPaymentMethod((err, payload) => {
-                if (err) {
-                    console.error('Errore nel richiedere il metodo di pagamento:', err);
-                    this.errorMessage = 'Errore nel richiedere il metodo di pagamento. ' + err.message;
-                    const errorModal = new Modal(document.getElementById('paymentErrorModal'));
-                    errorModal.show();
-                    return;
-                }
-
-                axios.post('/api/payment/process', {
-                    payment_method_nonce: payload.nonce,
-                    amount: this.total.toFixed(2),
-                })
-                    .then((response) => {
-                        this.transactionId = response.data.transaction_id;
-
-                        // mostra la modal di bootstrap per il pagamento riuscito
-                        const successModal = new Modal(document.getElementById('paymentSuccessModal'));
-                        successModal.show();
-                    })
-                    .catch((error) => {
-                        console.error('Errore nel processare il pagamento:', error);
-                        this.errorMessage = 'Errore nel processare il pagamento. ' + error.message;
-                        const errorModal = new Modal(document.getElementById('paymentErrorModal'));
-                        errorModal.show();
-                    });
-            });
-        },
+            return;
+          }
+          this.dropinInstance = instance;
+        }
+      );
     },
+    processPayment() {
+      if (!this.dropinInstance) {
+        console.error(
+          "L'istanza di Braintree Drop-In non è stata inizializzata."
+        );
+        return;
+      }
+
+      this.dropinInstance.requestPaymentMethod((err, payload) => {
+        if (err) {
+          console.error("Errore nel richiedere il metodo di pagamento:", err);
+          this.errorMessage =
+            "Errore nel richiedere il metodo di pagamento. " + err.message;
+          const errorModal = new Modal(
+            document.getElementById("paymentErrorModal")
+          );
+          errorModal.show();
+          return;
+        }
+
+        axios
+          .post("/api/payment/process", {
+            payment_method_nonce: payload.nonce,
+            amount: this.total.toFixed(2),
+          })
+          .then((response) => {
+            this.transactionId = response.data.transaction_id;
+            const successModal = new Modal(
+              document.getElementById("paymentSuccessModal")
+            );
+            successModal.show();
+          })
+          .catch((error) => {
+            console.error("Errore nel processare il pagamento:", error);
+            this.errorMessage =
+              "Errore nel processare il pagamento. " + error.message;
+            const errorModal = new Modal(
+              document.getElementById("paymentErrorModal")
+            );
+            errorModal.show();
+          });
+      });
+    },
+  },
 };
 </script>
-  
 
-<style scoped></style> 
+<style scoped></style>
--- src/pages/PayPage.vue
+++ src/pages/PayPage.vue
@@ -1,67 +1,59 @@
-<script>
-import UserFormInfo from '../components/apiComponents/UserFormInfo.vue';
-import BraintreePayment from '../components/apiComponents/BraintreePayment.vue';
-
-export default {
-    name: 'PayPage',
-    components: {
-        UserFormInfo,
-        BraintreePayment
-    },
-    data() {
-        return {
-            cart: [],
-            totale: null,
-            formData: null,  // Memorizza i dati del form
-        }
-    },
-    mounted() {
-        if (localStorage.getItem('cart')) {
-            this.cart = JSON.parse(localStorage.getItem('cart'));
-            this.totale = JSON.parse(localStorage.getItem('tot'));
-        }
-    },
-    methods: {
-        handleFormSubmit(formData) {
-            // Salva i dati del form
-            this.formData = formData;
-
-            // Procedi con il pagamento
-            this.$refs.braintreePayment.processPayment()
-                .then(() => {
-                    // Se il pagamento ha successo, invia i dati del form al backend
-                    axios.post('/api/order', this.formData)
-                        .then(response => {
-                            console.log('Ordine inviato con successo:', response.data);
-                        })
-                        .catch(error => {
-                            console.error('Errore nell\'invio dell\'ordine:', error);
-                        });
-                })
-                .catch(error => {
-                    console.error('Errore nel pagamento:', error);
-                });
-        }
-    }
-}
-</script>
-
 <template>
-    <div class="container bg-white">
+  <div class="container bg-white">
+    <div class="row">
+      <div class="col-12">
+        <p v-for="dish in cart" :key="dish.id">
+          {{ dish.name }} <span class="ms-2">x{{ dish.quantity }}</span>
+        </p>
+        <p class="mt-5">Prezzo totale: €{{ parseFloat(totale).toFixed(2) }}</p>
+      </div>
+      <div class="container">
         <div class="row">
-            <col-12>
-                <p v-for="dish in cart" :key="dish.id">{{ dish.name }} <span class="ms-2">x{{ dish.quantity }}</span></p>
-                <p class="mt-5">Prezzo totale: €{{ parseFloat(totale).toFixed(2) }} </p>
-            </col-12>
-            <div class="container">
-                <div class="row">
-                    <!-- Passa l'evento submit form -->
-                    <UserFormInfo @formSubmitted="handleFormSubmit"></UserFormInfo>
-                    <BraintreePayment ref="braintreePayment" :total="totale"></BraintreePayment>
-                </div>
-            </div>
+          <!-- Passa l'evento submit form -->
+          <UserFormInfo @formSubmitted="handleFormSubmit"></UserFormInfo>
+          <BraintreePayment
+            ref="braintreePayment"
+            :total="totale"
+            :formData="formData"></BraintreePayment>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<script>
+import UserFormInfo from "../components/apiComponents/UserFormInfo.vue";
+import BraintreePayment from "../components/apiComponents/BraintreePayment.vue";
+
+export default {
+  name: "PayPage",
+  components: {
+    UserFormInfo,
+    BraintreePayment,
+  },
+  data() {
+    return {
+      cart: [],
+      totale: null,
+      formData: null, // Memorizza i dati del form
+    };
+  },
+  mounted() {
+    if (localStorage.getItem("cart")) {
+      this.cart = JSON.parse(localStorage.getItem("cart"));
+      this.totale = JSON.parse(localStorage.getItem("tot"));
+    }
+  },
+  methods: {
+    handleFormSubmit(formData) {
+      // Salva i dati del form
+      this.formData = formData;
+
+      // Procedi con il pagamento
+      this.$refs.braintreePayment.processPayment();
+    },
+  },
+};
+</script>
+
+<style scoped></style>
