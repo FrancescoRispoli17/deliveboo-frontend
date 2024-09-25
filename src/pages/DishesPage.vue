fbfd97765f84f07757acb0fdd09c06281b000c90
rinox3005 <peppe.rinox@gmail.com>
2024-09-25 18:58:56 +0200

    [FIX] fixed change cart modal style and message

--- src/pages/DishesPage.vue
+++ src/pages/DishesPage.vue
@@ -1,8 +1,8 @@
 <script>
 import axios from "axios";
 import { store } from "../store";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Modal } from 'bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Modal } from "bootstrap";
 
 // Importa i componenti header e footer
 import HeaderEmptyComponent from "../components/headers/HeaderEmptyComponent.vue";
@@ -21,7 +21,7 @@ export default {
     return {
       restaurant: {}, // Dettagli del ristorante
       totale: null,
-      lastName:null,
+      lastName: null,
       store, // Totale prezzo carrello
     };
   },
@@ -42,13 +42,18 @@ export default {
               this.store.cart = JSON.parse(localStorage.getItem("cart")); // Recupera carrello
               this.totale = JSON.parse(localStorage.getItem("tot")); // Recupera totale
             } else {
-              this.store.cart = []; 
-              this.lastName=JSON.parse(localStorage.getItem("lastRestaurantName"));
+              this.store.cart = [];
+              this.lastName = JSON.parse(
+                localStorage.getItem("lastRestaurantName")
+              );
             }
           }
         })
         .catch((error) => {
-          console.error("Errore nel recupero dei piatti del ristorante:",error);
+          console.error(
+            "Errore nel recupero dei piatti del ristorante:",
+            error
+          );
         });
     },
 
@@ -59,9 +64,8 @@ export default {
         localStorage.getItem("lastRestaurant") != this.restaurant.id
       ) {
         // Conferma di svuotare il carrello precedente
-        const myModal = new Modal(document.getElementById('confirm'));
+        const myModal = new Modal(document.getElementById("confirm"));
         myModal.show();
-
       } else {
         this.addToCart(dish); // Aggiunge il piatto se è lo stesso ristorante
       }
@@ -94,8 +98,14 @@ export default {
 
       // Aggiorna il localStorage con i dati del carrello
       localStorage.setItem("cart", JSON.stringify(this.store.cart));
-      localStorage.setItem("lastRestaurant",JSON.stringify(this.restaurant.id));
-      localStorage.setItem("lastRestaurantName",JSON.stringify(this.restaurant.name));
+      localStorage.setItem(
+        "lastRestaurant",
+        JSON.stringify(this.restaurant.id)
+      );
+      localStorage.setItem(
+        "lastRestaurantName",
+        JSON.stringify(this.restaurant.name)
+      );
       this.calcoloTotale();
       this.store.lastCart = this.store.cart;
     },
@@ -142,10 +152,14 @@ export default {
 <template>
   <HeaderEmptyComponent />
   <div class="margin">
-    <div class="container py-5" >
+    <div class="container py-5">
       <div class="mb-3 margin-top">
-        <router-link class="text-decoration-none fw-bold d-flex align-items-center" :to="{ name: 'restaurant' }">
-          <FontAwesomeIcon :icon="['fas', 'arrow-left']" class="me-2 icon go-back" />
+        <router-link
+          class="text-decoration-none fw-bold d-flex align-items-center"
+          :to="{ name: 'restaurant' }">
+          <FontAwesomeIcon
+            :icon="['fas', 'arrow-left']"
+            class="me-2 icon go-back" />
           <span class="go-back">Pagina ristoranti</span>
         </router-link>
       </div>
@@ -153,7 +167,9 @@ export default {
       <div class="row" v-if="restaurant">
         <div class="col-md-4 mb-3">
           <div class="img-container">
-            <img :src="restaurant.image_path_url || '/restaurant-placeholder.jpg'" alt="Immagine del ristorante"
+            <img
+              :src="restaurant.image_path_url || '/restaurant-placeholder.jpg'"
+              alt="Immagine del ristorante"
               class="img" />
           </div>
         </div>
@@ -169,32 +185,53 @@ export default {
 
   <div class="container py-3 mb-5">
     <div class="row">
-      <div class="col-lg-8 py-3 scroll margin-phone" v-if="restaurant.dishes && restaurant.dishes.length">
+      <div
+        class="col-lg-8 py-3 scroll margin-phone"
+        v-if="restaurant.dishes && restaurant.dishes.length">
         <div class="row">
-          <div v-for="dish in restaurant.dishes.filter(
-            (dish) => dish.visible === 1)" :key="dish.id" class="card-custom mb-3 me-2" style="height: 100%">
+          <div
+            v-for="dish in restaurant.dishes.filter(
+              (dish) => dish.visible === 1
+            )"
+            :key="dish.id"
+            class="card-custom mb-3 me-2"
+            style="height: 100%">
             <div class="row">
-              <div class="col-md-2 py-5 dish-image" :style="{
-                backgroundImage: `url(${dish.image_path_url || '/dish-placeholder.jpg'
+              <div
+                class="col-md-2 py-5 dish-image"
+                :style="{
+                  backgroundImage: `url(${
+                    dish.image_path_url || '/dish-placeholder.jpg'
                   })`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-              }"></div>
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                }"></div>
 
-              <div style="height: 175px;" class="col-md-8 py-2 card-body-container">
+              <div
+                style="height: 175px"
+                class="col-md-8 py-2 card-body-container">
                 <div class="card-body">
                   <h5 class="card-title mb-3">{{ dish.name }}</h5>
                   <p class="card-text">{{ dish.description }}</p>
-                  <div class="d-flex align-items-center justify-content-between">
+                  <div
+                    class="d-flex align-items-center justify-content-between">
                     <p class="card-text">{{ dish.price }}€</p>
                     <div class="ms-5 d-flex gap-4">
                       <span v-if="dish.gluten_free === 1">
-                        <FontAwesomeIcon :icon="['fas', 'wheat-awn-circle-exclamation']" size="xs"
+                        <FontAwesomeIcon
+                          :icon="['fas', 'wheat-awn-circle-exclamation']"
+                          size="xs"
                           style="color: #7a7a7aba" />
                       </span>
-                      <span v-if="dish.lactose_free === 1" style="position: relative; display: inline-block">
-                        <FontAwesomeIcon :icon="['fas', 'cow']" size="xs" style="color: #7a7a7aba" />
-                        <span style="
+                      <span
+                        v-if="dish.lactose_free === 1"
+                        style="position: relative; display: inline-block">
+                        <FontAwesomeIcon
+                          :icon="['fas', 'cow']"
+                          size="xs"
+                          style="color: #7a7a7aba" />
+                        <span
+                          style="
                             position: absolute;
                             top: 9px;
                             left: -3px;
@@ -207,36 +244,72 @@ export default {
                           "></span>
                       </span>
                       <span v-if="dish.spicy === 1">
-                        <FontAwesomeIcon :icon="['fas', 'pepper-hot']" size="xs" style="color: #7a7a7aba" />
+                        <FontAwesomeIcon
+                          :icon="['fas', 'pepper-hot']"
+                          size="xs"
+                          style="color: #7a7a7aba" />
                       </span>
                       <span v-if="dish.vegan === 1">
-                        <FontAwesomeIcon :icon="['fas', 'leaf']" size="xs" style="color: #7a7a7aba" />
+                        <FontAwesomeIcon
+                          :icon="['fas', 'leaf']"
+                          size="xs"
+                          style="color: #7a7a7aba" />
                       </span>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="col-md-2 py-3 d-flex justify-content-center align-items-center btn button buy-cta" @click="confirim(dish)">
-                <font-awesome-icon :icon="['fas', 'cart-shopping']" class="text-white" />
+              <div
+                class="col-md-2 py-3 d-flex justify-content-center align-items-center btn button buy-cta"
+                @click="confirim(dish)">
+                <font-awesome-icon
+                  :icon="['fas', 'cart-shopping']"
+                  class="text-white" />
               </div>
               <!-- Modale -->
-              <div class="modal fade" id="confirm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h1 class="title fs-5" id="exampleModalLabel">Vuoi creare un nuovo carrello </h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body">
-                        <p>In questo modo cancelli il carrello esistente da {{lastName}} e crei un nuovo carrelllo da {{ restaurant.name }} </p>
-                      </div>
-                      <div class="modal-footer d-flex justify-content-center">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                        <button type="button" class="btn btn-primary" @click="addToCart(dish)" data-bs-dismiss="modal">Continua</button>
-                      </div>
+              <div
+                class="modal fade"
+                id="confirm"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="title fs-5" id="exampleModalLabel">
+                        Vuoi creare un nuovo carrello?
+                      </h1>
+                      <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <p>
+                        Cosí eliminerai il tuo precedente ordine da
+                        <strong>{{ lastName }}</strong> e creerai uno nuovo da
+                        <strong>{{ restaurant.name }}</strong>
+                      </p>
+                    </div>
+                    <div class="modal-footer d-flex justify-content-center">
+                      <button
+                        type="button"
+                        class="px-3 bg-custom-secondary fs-6 border border-none me-2 mt-2 text-white"
+                        data-bs-dismiss="modal">
+                        Annulla
+                      </button>
+                      <button
+                        type="button"
+                        class="px-3 bg-custom-primary fs-6 border border-none me-2 mt-2 text-white"
+                        @click="addToCart(dish)"
+                        data-bs-dismiss="modal">
+                        Continua
+                      </button>
                     </div>
                   </div>
                 </div>
+              </div>
             </div>
           </div>
         </div>
@@ -268,11 +341,15 @@ export default {
               <hr />
             </div>
             <div class="col-6 d-flex justify-content-end py-1">
-              <button class="btn recap-buttons text-white me-3" style="font-weight: 500; height: 40px; width: 40px"
+              <button
+                class="btn recap-buttons text-white me-3"
+                style="font-weight: 500; height: 40px; width: 40px"
                 @click="deleteSingleDish(dish, index)">
                 -
               </button>
-              <button class="btn recap-buttons text-white ms-3" style="font-weight: 500; height: 40px; width: 40px"
+              <button
+                class="btn recap-buttons text-white ms-3"
+                style="font-weight: 500; height: 40px; width: 40px"
                 @click="addToCart(dish)">
                 +
               </button>
@@ -282,31 +359,52 @@ export default {
         <div v-if="store.cart.length" class="text-center total-static-cart">
           <p class="total">Totale: €{{ totale }}</p>
           <div class="d-flex justify-content-center">
-            <button type="button" class="btn button-cart" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <button
+              type="button"
+              class="btn button-cart"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal">
               Svuota ordine
             </button>
-            <router-link :to="{ name: 'payPage' }" class="btn button-cart mx-3">Conferma ordine</router-link>
+            <router-link :to="{ name: 'payPage' }" class="btn button-cart mx-3"
+              >Conferma ordine</router-link
+            >
           </div>
         </div>
         <div v-else class="d-flex justify-content-center align-items-center">
-          <span class="text-empty-cart" style="font-weight: 800">Di cosa hai voglia?</span>
+          <span class="text-empty-cart" style="font-weight: 800"
+            >Di cosa hai voglia?</span
+          >
         </div>
       </div>
     </div>
 
     <!-- Modale -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
             <h1 class="title fs-5" id="exampleModalLabel">Svuota ordine</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"></button>
           </div>
           <div class="modal-body">
             <p>Sei sicuro di voler eliminare l'ordine in corso?</p>
           </div>
           <div class="modal-footer d-flex justify-content-center">
-            <button type="button" class="btn modal-button" @click="deleteCart()" data-bs-dismiss="modal">
+            <button
+              type="button"
+              class="btn modal-button"
+              @click="deleteCart()"
+              data-bs-dismiss="modal">
               Svuota
             </button>
           </div>
@@ -323,7 +421,23 @@ export default {
 @use "src/assets/partials/_variables.scss" as *;
 @use "src/assets/partials/_mixin.scss" as *;
 
-.margin-top{
+.bg-custom-primary {
+  background-color: $primary-color;
+  border-radius: 0.5rem;
+  text-decoration: none;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+.bg-custom-secondary {
+  background-color: $secondary-color;
+  border-radius: 0.5rem;
+  text-decoration: none;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+.margin-top {
   margin-top: 4rem;
 }
 
@@ -472,7 +586,6 @@ export default {
 }
 
 @media (max-width: 769px) {
-
   //card
   .card-custom {
     width: 100%;
